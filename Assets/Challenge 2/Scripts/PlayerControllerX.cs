1d77aba61fc068527f8f164d7743456bdbac6238
non0n0no <a55120@alunos.ipb.pt>
2024-06-12 01:28:47 +0100

    Challenge 2- Play fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    float DogTimer;
+
+    [SerializeField] private float dogCooldown = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DogTimer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && DogTimer > dogCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            DogTimer = 0f;
+        }
+    }
+}
